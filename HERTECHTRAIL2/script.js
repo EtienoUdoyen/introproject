468d97e11fc0b3a941710eff7c64ab04bc53ea22
Etieno Udoyen <162733507+EtienoUdoyen@users.noreply.github.com>
2025-07-18 14:58:42 +0100

    Add files via upload

---
+++ HERTECHTRAIL2/script.js
@@ -0,0 +1,48 @@
+const change = document.getElementById("Afia-Efere");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "green";
+});
+
+const change2 = document.getElementById("art");
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "red";
+});
+
+const change3 = document.getElementById("bag");
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "pink";
+});
+
+const change4 = document.getElementById("car");
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("bible");
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "silver";
+    event.target.style.color = "gold";
+});
+
+const change6 = document.getElementById("forest");
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "violet";
+    event.target.style.color = "orange";
+});
+
+const change7 = document.getElementById("house");
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "springgreen";
+    event.target.style.color = "maroon";
+});
+
+const change8 = document.getElementById("star");
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "blanchedalmond";
+});
